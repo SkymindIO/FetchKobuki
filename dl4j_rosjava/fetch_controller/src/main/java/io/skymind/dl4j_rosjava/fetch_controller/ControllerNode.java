8db74b0fb80227aa2e0cebece9e3ff7c920e3666
Samuel Audet <samuel.audet@gmail.com>
2017-05-23 20:51:35 +0900

    Compute fetch<->kobuki/wall distances from gazebo/model_states messages

--- dl4j_rosjava/fetch_controller/src/main/java/io/skymind/dl4j_rosjava/fetch_controller/ControllerNode.java
+++ dl4j_rosjava/fetch_controller/src/main/java/io/skymind/dl4j_rosjava/fetch_controller/ControllerNode.java
@@ -18,6 +18,10 @@
 
 package io.skymind.dl4j_rosjava.fetch_controller;
 
+import geometry_msgs.Point;
+import geometry_msgs.Pose;
+import geometry_msgs.Quaternion;
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import org.apache.commons.logging.Log;
 import org.ros.concurrent.CancellableLoop;
@@ -37,6 +41,8 @@ import org.ros.node.topic.Subscriber;
  */
 public class ControllerNode extends AbstractNodeMain {
 
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+
     @Override
     public GraphName getDefaultNodeName() {
         return GraphName.of("dl4j_rosjava/fetch_controller");
@@ -49,7 +55,97 @@ public class ControllerNode extends AbstractNodeMain {
         subscriber.addMessageListener(new MessageListener<sensor_msgs.LaserScan>() {
             @Override
             public void onNewMessage(sensor_msgs.LaserScan message) {
-                log.info("ranges: " + Arrays.toString(message.getRanges()));
+                String ranges = "[";
+                for (float range : message.getRanges()) {
+                    if (ranges.length() > 1) {
+                        ranges += ", ";
+                    }
+                    ranges += df.format(range);
+                }
+                ranges += "]";
+                log.info("ranges: " + ranges);
+            }
+        });
+
+        Subscriber<gazebo_msgs.ModelStates> subscriber2 = connectedNode.newSubscriber("gazebo/model_states", gazebo_msgs.ModelStates._TYPE);
+        subscriber2.addMessageListener(new MessageListener<gazebo_msgs.ModelStates>() {
+            @Override
+            public void onNewMessage(gazebo_msgs.ModelStates message) {
+                int count = 0, wallCount = 0;
+                int fetchIndex = -1, kobukiIndex = -1;
+                int[] wallIndices = new int[4];
+                String names = "";
+                for (String name : message.getName()) {
+                    if (name.equals("fetch")) {
+                        fetchIndex = count;
+                    } else if (name.equals("kobuki")) {
+                        kobukiIndex = count;
+                    } else if (name.contains("wall")) {
+                        wallIndices[wallCount] = count;
+                        wallCount++;
+                    }
+                    names += name + ", ";
+                    count++;
+                }
+                log.info("names: " + names);
+
+                count = 0;
+                Pose fetchPose = null, kobukiPose = null;
+                Pose[] wallPoses = new Pose[4];
+                String poses = "";
+                for (Pose pose : message.getPose()) {
+                    if (count == fetchIndex) {
+                        fetchPose = pose;
+                    } else if (count == kobukiIndex) {
+                        kobukiPose = pose;
+                    } else for (int i = 0; i < wallPoses.length; i++) {
+                        if (count == wallIndices[i]) {
+                            wallPoses[i] = pose;
+                        }
+                    }
+                    Point p = pose.getPosition();
+                    Quaternion q = pose.getOrientation();
+                    if (poses.length() > 1) {
+                        poses += ", ";
+                    }
+                    poses += "[" + df.format(p.getX()) + ", " + df.format(p.getY()) + ", " + df.format(p.getZ())
+                          + "; " + df.format(q.getX()) + ", " + df.format(q.getY()) + ", " + df.format(q.getZ())
+                          + ", " + df.format(q.getW()) + "]";
+                    count++;
+                }
+                log.info("poses: " + poses);
+
+                double x1 = fetchPose.getPosition().getX();
+                double y1 = fetchPose.getPosition().getY();
+                double x2 = kobukiPose.getPosition().getX();
+                double y2 = kobukiPose.getPosition().getY();
+                double kobukiDist = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+                log.info("kobukiDist: " + df.format(kobukiDist));
+
+                count = 0;
+                double[] wallDists = new double[4];
+                org.ros.rosjava_geometry.Vector3 referenceAxis
+                        = new org.ros.rosjava_geometry.Vector3(0.0, 1.0, 0.0);
+                for (Pose wallPose : wallPoses) {
+                    Point p = wallPose.getPosition();
+                    org.ros.rosjava_geometry.Vector3 p2
+                            = new org.ros.rosjava_geometry.Vector3(p.getX(), p.getY(), p.getZ());
+                    Quaternion q = wallPose.getOrientation();
+                    org.ros.rosjava_geometry.Quaternion q2
+                            = new org.ros.rosjava_geometry.Quaternion(q.getX(), q.getY(), q.getZ(), q.getW());
+                    org.ros.rosjava_geometry.Vector3 n = q2.rotateAndScaleVector(referenceAxis);
+
+                    // compute minimum distance from Fetch to the line of the wall
+                    double a = n.getX();
+                    double b = n.getY();
+                    double c = n.getZ(); // should be pretty close to 0 for a wall
+                    double d = -n.dotProduct(p2);
+                    wallDists[count] = Math.abs(a * x1 + b * y1 + d);// / Math.sqrt(a * a + b * b);
+                    count++;
+                }
+                log.info("wallDists: [" + df.format(wallDists[0]) + ", " + df.format(wallDists[1])
+                                 + ", " + df.format(wallDists[2]) + ", " + df.format(wallDists[3]) + "]");
+
             }
         });
 
